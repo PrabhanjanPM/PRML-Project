79a3e4a69b3a7d9d12455bfd3ab1fc95035b8887
Prabhanjan Mannari <PrabhanjanMannari@gmail.com>
2017-04-09 20:59:15 +0530

    add legends

--- assignment_2/gmm_learning_curves.py
+++ assignment_2/gmm_learning_curves.py
@@ -6,7 +6,8 @@ step = 150
 number_of_samples = step*np.array([1,2,3,4,5,6,7,8,9,10])
 train_costs = np.loadtxt("gmm_train_costs")
 test_costs  = np.loadtxt("gmm_test_costs")
-plt.plot(number_of_samples, train_costs)
-plt.plot(number_of_samples, test_costs)
+plt.plot(number_of_samples, train_costs, label='training curve')
+plt.plot(number_of_samples, test_costs, label='testing curve')
+plt.legend(loc='center right')
 plt.show()
 
